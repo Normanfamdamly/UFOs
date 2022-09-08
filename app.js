19676b0374fbb414ce31840dec8bc07288a346f5
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-09-08 17:59:35 -0400

    11.3.2

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 const tableData=data;
 //  Reference the HTML tabel using d3
 var tbody =d3.select("tbody");
+// Simple JavaScript console.log statement
+function printHello(){
+    console.log("Hello there!");
+}
